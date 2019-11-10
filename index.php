dfb98a4200d26244fb9ee0f7f637a44f00094c3d
Yahmig <yvan.gimard@gmail.com>
2019-11-10 19:19:14 +0100

    dzq

--- index.php
+++ index.php
@@ -6,113 +6,122 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="style/reset.css" rel="stylesheet" type="text/css">
     <link href="style/style.css" rel="stylesheet" type="text/css">
-    <title>Document</title>
+    <title>Site Gimard</title>
 </head>
 <body>
     <div id="containerPage">
         <div id="moitiePageGauche">
-            <div id="selectionLeague">
-                <div class="league">
-                    <img src="" alt="">
-                    <span class="rectSelect"></span>
-                </div>
-                <div class="league">
-                    <img src="" alt="">
-                    <span class="rectSelect"></span>
-                </div>
-                <div class="league">
-                    <img src="" alt="">
-                    <span class="rectSelect"></span>
-                </div>
-                <div class="league">
-                    <img src="" alt="">
-                    <span class="rectSelect"></span>
-                </div>
-                <div class="league">
-                    <img src="" alt="">
-                    <span class="rectSelect"></span>
-                </div>
-            </div>
-            <div id="searchBar">
-
-            </div>
-            <div id="blocLogos">
-                <div class="ligneClub">
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
+            <div class="wrap">
+                <div id="selectionLeague">
+                    <div class="league">
+                        <div>
+                        <img src="image/premier_league.png" alt="Logo premier league">
+                        </div>
+                        <span class="rectSelect"></span>
                     </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
+                    <div class="league">
+                        <img src="image/premier_league.png" alt="Logo premier league">
+                        <span class="rectSelect"></span>
                     </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
+                    <div class="league">
+                    <img src="image/premier_league.png" alt="Logo premier league">
+                        <span class="rectSelect"></span>
                     </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
+                    <div class="league">
+                        <img src="image/premier_league.png" alt="Logo premier league">
+                        <span class="rectSelect"></span>
+                    </div>
+                    <div class="league">
+                        <img src="image/premier_league.png" alt="Logo premier league">
+                        <span class="rectSelect"></span>
                     </div>
                 </div>
-                <div class="ligneClub">
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
+                <div id="sectionClubs">
+                    <div id="searchBar">
+                        <h1>Search club</h1>
                     </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
-                    </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
-                    </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
-                    </div>
+                    <div class="Rectangle"></div>
+                    <div id="blocLogos">
+                        <div class="ligneClub">
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                        </div>
+                        <div class="ligneClub">
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                        </div>
+                        <div class="ligneClub">
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                        </div>
+                        <div class="ligneClub">
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                            <div class="blocClub">
+                                <img src="" alt="">
+                                    <p></p>
+                            </div>
+                        </div>                
+                    </div> 
                 </div>
-                <div class="ligneClub">
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
-                    </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
-                    </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
-                    </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
-                    </div>
-                </div>
-                <div class="ligneClub">
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
-                    </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
-                    </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
-                    </div>
-                    <div class="blocClub">
-                        <img src="" alt="">
-                            <p></p>
-                    </div>
-                </div>                
-            </div> 
-        </div>
+            </div> <!-- fin wrap !-->
+        </div> <!-- fin moitie gauche !-->
         <div class="moitiePageDroite">
+            <div class="wrap">
 
+            </div>
         </div>
     </div>    
 
