272eb74d32c9112b2d9530cb7fae58264cf91bac
Yvan <yvan.gimard@gmail.com>
2019-12-13 09:50:40 +0100

    up

---
+++ www/script/script.js
@@ -0,0 +1,15 @@
+
+
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://localhost/API_football/index.php",
+    "method": "GET",
+    "headers": {
+      "x-auth-token": "6a895e1cccb14f07b90701ec3a5dc38a"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
