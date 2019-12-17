f6030e06998de2affb39465fd66814533649dcf8
Yvan <yvan.gimard@gmail.com>
2019-12-17 17:09:49 +0100

    up

--- www/script/script.js
+++ www/script/script.js
@@ -1,9 +1,12 @@
 
 
+
+
+//Lancer une requête via l'api
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "http://localhost/API_football/index.php",
+    "url": "http://localhost/API_football/www/index.php",
     "method": "GET",
     "headers": {
       "x-auth-token": "6a895e1cccb14f07b90701ec3a5dc38a"
